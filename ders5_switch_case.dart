6d7880592de02b97f1215f3075835652f632fb49
akkayasoft <akkayasoft@gmail.com>
2024-07-02 06:28:30 +0300

    ders5 ve 6 bitti

---
+++ ders5_switch_case.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  int gun=5;
+  switch(gun){
+    case 1:print("Pazartesi");break;
+    case 2:"Salı";break;
+    case 3:"Çarşamba";break;
+    case 4:"Perşembe";break;
+    case 5:print("Cuma");break;
+    case 6:"Cumartesi";break;
+    case 7:"Pazar";break;
+    default:"1-7 arasında değer girin";break;
+  }
+
+var isim="admin";
+switch(isim){
+  case "admin":print("admin giriş yaptı");break;
+  case "guest":print("Misafir giriş yaptı");break;
+  case "user":print("Kullanıcı giriş yaptı");break;
+}
+}
---
+++ ders6_donguler.dart
@@ -0,0 +1,36 @@
+void main(List<String> args) {
+  //for döngüsü
+  // for(int i=0;i<10;i++){
+  //   print(i);
+  // }
+
+  //while döngüsü
+
+  // int a=0;
+  // while(a<10){ //küçük iken
+  //   print(a);
+  //   a++;
+  // }
+
+  //foreach döngüsü
+
+  // var liste=["urfa","adana","maraş","antep","van"];
+  // liste.forEach((i) { 
+  //   print(i);
+  // });
+  // var liste=["urfa","adana","maraş","antep","van"];
+  // for(var item in liste){
+  //   print(item);
+  // }
+
+
+// do-while döngüsü
+
+int i=12;
+do{
+  print(i);
+  i--;
+
+}while(i>10);
+
+}
